31ec6ab0ad7578b87d19dbb05e44fa624534d759
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2023-12-11 16:43:23 +0530

    Server

---
+++ serever.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 3000;
+
+http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Welcome to HTTP server");
+});
+
+Server.listen(port, hostname, () => {
+  console.log("server running at http://$(hostname):&(port)/");
+});
