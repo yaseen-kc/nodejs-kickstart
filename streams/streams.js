30b197b84caa5704597c3c12f49b0386ad02d6ff
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2023-12-13 21:41:16 +0530

    addedCommentsStream

--- streams/streams.js
+++ streams/streams.js
@@ -1,9 +1,17 @@
+// Importing the 'http' and 'fs' modules for creating an HTTP server and reading files.
 const http = require("http");
 const fs = require("fs");
 
+// Creating an HTTP server that streams the contents of "test.json" to the response.
 const server = http.createServer(function (req, res) {
+    // Creating a readable stream from the file "test.json".
     const stream = fs.createReadStream("test.json");
-    stream.pipe(res);
-})
 
-server.listen(3000, () => { console.log("Application Started") });
+    // Piping the stream to the response object, sending the file content to the client.
+    stream.pipe(res);
+});
+
+// Listening on port 3000 for incoming HTTP requests.
+server.listen(3000, () => {
+    console.log("Application Started");
+});
