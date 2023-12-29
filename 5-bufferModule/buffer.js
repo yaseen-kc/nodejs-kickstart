380c5c0782edae31cb270d4b2f9f8a45af59a9b2
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2023-12-29 10:16:50 +0530

    Buffer Module update sample.txt

---
+++ 5-bufferModule/buffer.js
@@ -0,0 +1,10 @@
+// Creating Buffer
+
+// Uninitiated Buffer
+const buffer = Buffer.alloc(10);
+
+// Array Buffer
+const bufferArray = Buffer.from([1, 2, 3, 4, 5])
+
+// String Buffer
+const bufferString = Buffer.from('Hello World', 'utf-8');
